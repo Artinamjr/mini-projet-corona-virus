c5ac422e4a81da7cdbec632a21860223ef9f5b17
Artinamjr <125258091+Artinamjr@users.noreply.github.com>
2023-02-13 14:51:51 +0300

    Add files via upload

---
+++ virs.js
@@ -0,0 +1,18 @@
+var compteurImage = 1;
+var totalimage = 10;
+
+function slider(x) {
+
+    var image = document.getElementById('img');
+    compteurImage = compteurImage + x;
+    image.src = "images4"
+    compteurImage + ".jpg";
+
+    if (compteurImage >= totalimage) {
+        c
+        compteurImage = 1;
+    }
+    if (condition) {
+
+    }
+}
